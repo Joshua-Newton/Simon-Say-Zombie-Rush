133a1b6891287543b3bfcb254baaa9b0d902328e
Joshua Newton <33265085+Joshua-Newton@users.noreply.github.com>
2024-07-08 13:37:32 -0400

    Add basic player jumping, shooting, sprinting

--- SimonSaysZombieRush/Assets/Scripts/Player.cs
+++ SimonSaysZombieRush/Assets/Scripts/Player.cs
@@ -7,10 +7,17 @@ public class Player : MonoBehaviour
     [SerializeField] CharacterController characterController;
     
     [SerializeField] int speed;
-    [SerializeField] int gravity;
-
+    [SerializeField] int sprintMultiplier;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpStrength;
+    [SerializeField] int maxJumps;
+    [SerializeField] float shootDelay;
+    [SerializeField] float shootRange;
+    [SerializeField] LayerMask ignoreLayer;
     Vector3 movementDirection;
     Vector3 playerVelocity;
+    int numJumps;
+    bool isShooting;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +28,72 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(characterController.isGrounded)
-        {
-            playerVelocity = Vector3.zero;
-        }
+        Movement();
+        Jump();
+        Sprint();
+        Shooting();
+    }
 
+    void Movement()
+    {
         movementDirection = Input.GetAxis("Vertical") * transform.forward +
                             Input.GetAxis("Horizontal") * transform.right;
 
         characterController.Move(movementDirection * speed * Time.deltaTime);
 
+    }
+
+    void Jump()
+    {
+        if (characterController.isGrounded)
+        {
+            numJumps = 0;
+            playerVelocity = Vector3.zero;
+        }
+
+        if (Input.GetButtonDown("Jump") && numJumps < maxJumps)
+        {
+            ++numJumps;
+            playerVelocity.y = jumpStrength;
+        }
+
         characterController.Move(playerVelocity * Time.deltaTime);
-        playerVelocity.y -= gravity;
+        playerVelocity.y -= gravity * Time.deltaTime;
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMultiplier;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMultiplier;
+        }
+    }
+
+    void Shooting()
+    {
+        if(Input.GetButton("Fire1") && !isShooting)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootRange, ~ignoreLayer))
+        {
+            Debug.Log(hit.collider.name);
+        }
+
+        yield return new WaitForSeconds(shootDelay);
+        isShooting = false;
 
     }
+
 }
