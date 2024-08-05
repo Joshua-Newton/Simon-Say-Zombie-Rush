8e801f538a23a88cf9798616938e58f9c5b7acbc
Joshua Newton <33265085+Joshua-Newton@users.noreply.github.com>
2024-08-05 17:12:26 -0400

    Add logic for camera to find player and adjust its own position/rotation based on Serialized Fields.

--- SimonSaysZombieRush/Assets/Scripts/PlayerScripts/CameraController.cs
+++ SimonSaysZombieRush/Assets/Scripts/PlayerScripts/CameraController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Range(1, 100)] [SerializeField] float distanceToPlayer = 10f;
+    [Range(0, 90)] [SerializeField] float angleToPlayer = 75f;
+
     GameObject player;
     Transform initialTransform;
     float initialZOffset;
@@ -11,8 +14,13 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initialTransform = transform;
         player = GameManager.instance.player;
+        Quaternion newRotation = Quaternion.identity;
+        newRotation = Quaternion.Euler(newRotation.x + angleToPlayer, newRotation.y, newRotation.z);
+        transform.SetPositionAndRotation(player.transform.position, newRotation);
+        transform.position -= transform.TransformDirection(Vector3.forward) * distanceToPlayer;
+        
+        initialTransform = transform;
         initialZOffset = transform.position.z - player.transform.position.z;
     }
 
