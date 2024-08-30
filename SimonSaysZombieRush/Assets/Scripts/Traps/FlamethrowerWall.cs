61461031d4b38c632ec8c035501b16386cbb9037
Joshua Newton <jonewton0905@gmail.com>
2024-08-30 14:05:18 -0400

    Fix Flamethrower trap hitbox being completely separate from particles

--- SimonSaysZombieRush/Assets/Scripts/Traps/FlamethrowerWall.cs
+++ SimonSaysZombieRush/Assets/Scripts/Traps/FlamethrowerWall.cs
@@ -1,30 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlamethrowerWall : MonoBehaviour
 {
-    [SerializeField] int flameRest;
-    [SerializeField] int flameTime;
-    [SerializeField] float sparkTime;
+    [SerializeField] int emissionRest = 2;
+    [SerializeField] float emissionTime = 1;
+    [SerializeField] float flameParticleLifetime = 1;
+    [SerializeField] float flameParticleSpeed = 5;
+    [SerializeField] float sparkTime = .5f;
     [SerializeField] Collider flameHitBox;
     [SerializeField] ParticleSystem flameSparks;
     [SerializeField] ParticleSystem flamethrower;
     bool isFlaming;
+    bool expanding;
+    
+    float farthesetDistance;
+    Transform flameHitBoxTransform;
+    BoxCollider flameHitBoxCollider;
+    Vector3 initialHitBoxSize;
+    private void Start()
+    {
+        ParticleSystem.MainModule mainSettings = flamethrower.main;
+        mainSettings.startLifetime = flameParticleLifetime;
+        mainSettings.startSpeed = flameParticleSpeed;
 
+        farthesetDistance = flameParticleLifetime * flameParticleSpeed;
+        flameHitBoxTransform = flameHitBox.gameObject.transform;
+        flameHitBoxCollider = flameHitBox.gameObject.GetComponent<BoxCollider>();
+        initialHitBoxSize = flameHitBoxCollider.size;
+    }
 
     // Update is called once per frame
     void Update()
     {
         flamethowerEmission();
+        expanding = flamethrower.isEmitting;
+        
+
     }
 
     public void flamethowerEmission()
     {
         if (!isFlaming)
         {
+            flameHitBox.gameObject.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
             StartCoroutine(sparksTimer());
             StartCoroutine(flamehitBoxTimer());
+        }
+        else if(expanding)
+        {
+            flameHitBoxCollider.center = Vector3.Lerp(flameHitBoxCollider.center, new Vector3(0, 0, farthesetDistance / 2), flameParticleSpeed/2 * Time.deltaTime);
+            flameHitBoxCollider.size = Vector3.Lerp(flameHitBoxCollider.size, new Vector3(initialHitBoxSize.x, initialHitBoxSize.y, farthesetDistance), flameParticleSpeed/2 * Time.deltaTime);
+        }
+        else
+        {
+            flameHitBoxCollider.center = Vector3.Lerp(flameHitBoxCollider.center, new Vector3(0, 0, farthesetDistance), flameParticleSpeed/2 * Time.deltaTime);
+            flameHitBoxCollider.size = Vector3.Lerp(flameHitBoxCollider.size, new Vector3(initialHitBoxSize.x, initialHitBoxSize.y, 0), flameParticleSpeed/2 * Time.deltaTime);
+            if (flamethrower.particleCount <= 0 || flameHitBoxCollider.size.z < 0.05f)
+            {
+                flameHitBox.gameObject.GetComponent<Damage>().DisableAndStopCoroutines();
+            }
         }
     }
 
@@ -33,10 +70,13 @@ public class FlamethrowerWall : MonoBehaviour
         flamethrower.Play(true);
         isFlaming = true;
         flameHitBox.gameObject.SetActive(true);
-        yield return new WaitForSeconds(flameTime);
+
+        yield return new WaitForSeconds(emissionTime);
         flamethrower.Stop();
-        flameHitBox.gameObject.GetComponent<Damage>().DisableAndStopCoroutines();
-        yield return new WaitForSeconds(flameRest);
+        
+        yield return new WaitForSeconds(emissionRest);
+        flameHitBoxCollider.size = initialHitBoxSize;
+        flameHitBoxCollider.center = Vector3.zero;
         isFlaming = false;
     }
 
